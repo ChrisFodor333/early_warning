94d49494eb378e0cdd5911fc668e0ccc5c706c43
ChrisFodor333 <chrisfodor333@gmail.com>
2021-11-17 12:30:40 +0100

    robot

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -32,32 +32,95 @@ class RegisterController extends Controller
       $url = file_get_contents('https://www.registeruz.sk/cruz-public/api/uctovne-vykazy?zmenene-od=2020-01-01&max-zaznamov=10000');
       $obj = json_decode($url);
       $myids = $obj->id;
-      $testid = "4546241";
+      $testid = "6881167";
 
 
       $url2 = file_get_contents('https://www.registeruz.sk/cruz-public/api/uctovny-vykaz?id=' . $testid);
       $json = json_decode($url2);
 
-      $aktiva = null;
-      $pasiva = null;
-      $income = null;
+      $aktiva_array = null;
+      $pasiva_array = null;
+      $income_statement_array = null;
       $mesto = null;
       $skNace = null;
       $currentyear = null;
 
+      $sorted_aktiva = array();
+      $sorted_pasiva = array();
+      $sorted_income_statement = array();
+
+      $lastyear_aktiva = array();
+      $lastyear_pasiva = array();
+      $lastyear_income = array();
+
+
 
       if($json->obsah->tabulky[0]->data ?? null) {
-      $aktiva = $json->obsah->tabulky[0]->data;
+      $aktiva_array = $json->obsah->tabulky[0]->data;
+      $i = -2;
+      foreach ($aktiva_array as $aktiva) {
+        if($i % 4 == 0) {
+        array_push($sorted_aktiva,$aktiva);
+        }
+        $i++;
       }
+
+      $i= 1;
+      foreach ($aktiva_array as $aktiva) {
+        if($i % 4 == 0) {
+        array_push($lastyear_aktiva,$aktiva);
+        }
+        $i++;
+      }
+      }
+
+
+
 
       if($json->obsah->tabulky[1]->data ?? null) {
-      $pasiva = $json->obsah->tabulky[1]->data;
+      $pasiva_array = $json->obsah->tabulky[1]->data;
+
+      $i = 0;
+      foreach ($pasiva_array as $pasiva) {
+        if($i % 2 == 0) {
+        array_push($sorted_pasiva,$pasiva);
+        }
+        $i++;
       }
+
+      $i = 1;
+      foreach ($pasiva_array as $pasiva) {
+        if($i % 2 == 0) {
+        array_push($lastyear_pasiva,$pasiva);
+        }
+        $i++;
+      }
+      }
+
+
+
+
 
       if($json->obsah->tabulky[2]->data ?? null) {
-      $income = $json->obsah->tabulky[2]->data;
+      $income_statement_array = $json->obsah->tabulky[2]->data;
+
+      $i = 0;
+      foreach ($income_statement_array as $income_statement) {
+        if($i % 2 == 0) {
+        array_push($sorted_income_statement,$income_statement);
+        }
+        $i++;
       }
 
+      $i = 1;
+      foreach ($income_statement_array as $income_statement) {
+        if($i % 2 == 0) {
+        array_push($lastyear_income,$income_statement);
+        }
+        $i++;
+      }
+
+      }
       if($json->obsah->titulnaStrana->adresa->mesto ?? null) {
       $mesto = $json->obsah->titulnaStrana->adresa->mesto;
       }
@@ -70,13 +133,9 @@ class RegisterController extends Controller
       $currentyear = substr($json->obsah->titulnaStrana->obdobieOd,0,4);
       }
 
-      print_r($mesto);
-
-
-
-
-      //$myarray = json_decode($url2, true);
-      //print_r($myarray);
+      if($aktiva_array != null && $pasiva_array != null && $income_statement_array != null) {
+        
+      }
 
     }
 
